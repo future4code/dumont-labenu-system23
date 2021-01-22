fd6e12767fe167ffcea1df8b9a3082f426bb46ab
Renata Caetano <renatapcaetano@hotmail.com>
2021-01-22 09:49:55 -0300

    correcao-ok

---
+++ src/data/insertMission.ts
@@ -0,0 +1,23 @@
+import { connection } from '../index';
+
+
+export default async function insertMission(
+    id:string,
+    name:string,
+    start_date:Date,
+    end_date:Date,
+    module?:string,
+    user_id?:string,
+   
+    ){
+        await connection.insert(
+            {
+                id,
+                name,
+                start_date,
+                end_date,
+                module,
+                user_id
+                  
+            }).into ('mission')
+    }
